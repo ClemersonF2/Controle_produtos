cd2ccd7b760a17bd460388e71af4487c06bca576
Dvdmeneses <144235213+dvdmeneses01@users.noreply.github.com>
2024-10-24 16:40:14 -0300

    banco.py

--- banco.py
+++ banco.py
@@ -4,4 +4,10 @@ con = lite.connect('pesagem.db')
 
 with con:
     cur=con.cursor()
-    cur.execute("CREATE TABLE produto (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, codigo INT, peso_liquido FLOAT, data_recebimento DATE, nota_fiscal INT)")
+    cur.execute("CREATE TABLE produto (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR,
+    codigo INT,
+    peso_liquido FLOAT,
+    data_recebimento DATE,
+    nota_fiscal INT)")
